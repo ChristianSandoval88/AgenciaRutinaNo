f6c9542ce08d2fa8959e91e62e0ddd1657cc3cdb
Christian Sandoval <christiansandoval@gmail.com>
2022-09-23 15:50:54 -0500

    Bugfixes

--- POS.Web/Controllers/KueskyController.cs
+++ POS.Web/Controllers/KueskyController.cs
@@ -37,7 +37,7 @@ public class KueskyController : Controller
             var requestStatus = "accept";
             if (request.status != "approved") requestStatus = "ok";
 
-            logger.LogInformation(requestStatus);
+            logger.LogInformation(requestStatus); 
             return Json(new { status = requestStatus });
         }
         return Json(new { status = "ok" });
